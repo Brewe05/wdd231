a7a108a32579b238331e062c929d965ebcd1a7ff
Brewe05 <brentwelsford@gmail.com>
2025-03-21 16:39:23 +0200

    final edit

--- scripts.js
+++ scripts.js
@@ -1,3 +1,8 @@
 import './toggleMenu.js';
 import './courses.js';
 import './footer.js';
+
+// Initial Display
+document.addEventListener("DOMContentLoaded", () => {
+    displayCourses(courses);
+});
