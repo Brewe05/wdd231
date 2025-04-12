6fed06d2a7809047341e87ae2d7b04d922963797
Brewe05 <brentwelsford@gmail.com>
2025-04-12 09:47:10 +0200

    6th week 4

--- chamber/scripts/join.js
+++ chamber/scripts/join.js
@@ -17,4 +17,13 @@ document.addEventListener("DOMContentLoaded", () => {
         const now = new Date();
         timestampField.value = now.toISOString();
     }
+
+    // Populate thank you page with form data
+    const urlParams = new URLSearchParams(window.location.search);
+    document.getElementById("first-name").textContent = urlParams.get("first-name");
+    document.getElementById("last-name").textContent = urlParams.get("last-name");
+    document.getElementById("email").textContent = urlParams.get("email");
+    document.getElementById("phone").textContent = urlParams.get("mobile-phone");
+    document.getElementById("organization").textContent = urlParams.get("organization");
+    document.getElementById("timestamp").textContent = urlParams.get("timestamp");
 });
